14fa01c07643f9fb40520c267b11c26ca365f8d9
Romain Roland <rolromain@hotmail.com>
2021-01-26 21:57:57 +0100

    add user and group

--- config.sh
+++ config.sh
@@ -1,3 +1,6 @@
 sudo dpkg --add-architecture i386; sudo apt update; sudo apt --assume-yes install curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl1.2debian libstdc++5:i386
 
-adduser ut2k4server
+sudo useradd -m ut2k4server
+sudo groupadd ut2k4server
+sudo usermod -a -G ut2k4server ut2k4server
+
