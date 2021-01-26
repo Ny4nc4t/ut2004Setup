5362971aa807a7a9d3a0d0a08f6006c5fbe64637
Romain Roland <rolromain@hotmail.com>
2021-01-26 21:07:09 +0100

    first commit

---
+++ config.sh
@@ -0,0 +1,3 @@
+sudo dpkg --add-architecture i386; sudo apt update; sudo apt --assume-yes install curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl1.2debian libstdc++5:i386
+
+adduser ut2k4server
